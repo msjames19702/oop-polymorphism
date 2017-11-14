ed895523f8e0c99893d61be03660d4af1178ef2d
Mike James <mike.james@dtcc.edu>
2017-11-14 10:39:33 -0500

    Add files via upload

--- oop-example-of-polymorphism-in-python-2.7.py
+++ oop-example-of-polymorphism-in-python-2.7.py
@@ -1,7 +1,12 @@
 
 # coding: utf-8
 
-# In[5]:
+# In[ ]:
+
+
+
+
+# In[25]:
 
 # Create Animal() superclass object.
 class Animal(object):
@@ -12,10 +17,10 @@ class Animal(object):
     
     def __init__(self):
         pass
-        
+    
     def whoAmi(self):
         print "Animal"
-        
+    
     def eat(self):
         print "Eating"
 
@@ -23,96 +28,75 @@ class Animal(object):
 class Dog(Animal):
     def __init__(self):
         Animal.__init__(self)
-        print "Dog subclass instance created"
-        
+        pass
+    
     def whoAmi(self):
         print "I'm a Dog"
-        
+    
     def sound(self):
         print "I go Woof!"
         
     def whatIdo(self):
         print "I play fetch the ball!"
+        
 
 # Create Cat() subclass object and functions. Make use of Inheritance.
 class Cat(Animal):
     def __init__(self):
         Animal.__init__(self)
-        print "Cat subclass instance created"
-        
+        pass
+    
     def whoAmi(self):
         print "I'm a Cat"
-        
+    
     def sound(self):
-        print "I go Meow!"
+        print "I go meow!"
         
     def whatIdo(self):
-        print "I play with yarn!"
+        print "I play with yard!"
 
-# Create Cow() subclass object and functions. Make use of Inheritance.
+# Create polymorphic object instances Dog() & Cat()
 
-class Cow(Animal):
-    def __init__(self):
-        Animal.__init__(self)
-        print "Cow subclass instance created"
-        
-    def whoAmi(self):
-        print "I'm a Cow"
-        
-    def sound(self):
-        print "I go Mmmmoooooo!"
-        
-    def whatIdo(self):
-        print "I just like to eat hay!"
-        
-# Create object instances Animal(), Dog(), Cat() and Cow()
-
-animalObject = Animal()
 dogObject = Dog()
 catObject = Cat()
-cowObject = Cow()
 tigerObject = Cat()
-        
+wolfObject = Dog()
+
 # Functional polymorphisms
 
 def whatAmi(animaltype):
     animaltype.whoAmi()
-    
+
 def soundImake(animalsound):
     animalsound.sound()
-    
+
 def iDo(animaldoes):
     animaldoes.whatIdo()
-
-# Calling objects as parameters to make use of polymorphism
-
-print"..."
+    
+# Calling the above functions with objects as arguments to illustrate polymorphism
+print ""
+print "Dog says ..."
 whatAmi(dogObject)
 soundImake(dogObject)
 iDo(dogObject)
-print "..."
-
+print ""
+print "Cat says ..."
 whatAmi(catObject)
 soundImake(catObject)
 iDo(catObject)
-print "..."
-
-whatAmi(cowObject)
-soundImake(cowObject)
-iDo(cowObject)
-print "..."
-
+print ""
+print "Tiger says ..."
 whatAmi(tigerObject)
 soundImake(tigerObject)
 iDo(tigerObject)
-print "..."
+print ""
+print "Wolf says ..."
+whatAmi(tigerObject)
+soundImake(tigerObject)
+iDo(tigerObject)
 
-# Making use of class objects but not using polymorphism
 
-print "Tiger says", whatAmi(tigerObject)
-print "Tiger says", soundImake(tigerObject)
-print "Tiger says", iDo(tigerObject)
-print "..."
-print "The species of a dog is "+dogObject.species
-print "Mammals are "+catObject.bloodtype
+# In[ ]:
+
+
 
